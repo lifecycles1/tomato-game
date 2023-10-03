559d6d8ba42976b591eea7812b89bacb6766debe
lifecycles1 <raichev89@gmail.com>
2023-10-03 18:19:05 +0100

    encryption added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 router.post("/signin", async (req, res) => {
@@ -8,7 +9,8 @@ router.post("/signin", async (req, res) => {
   try {
     const user = await User.findOne({ email: email });
     if (user) {
-      if (user.password === password) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
         res.status(200).send(user);
       } else {
         res.status(200).send("email or password are incorrect");
@@ -30,11 +32,14 @@ router.post("/signup", async (req, res) => {
     if (userExists) {
       res.status(200).send("email already exists");
     } else {
+      // Hash the password before storing it
+      const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
       const newUser = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
       });
       await newUser.save();
+
       res.status(201).send("account created");
     }
   } catch (error) {
